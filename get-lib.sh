c9299029bc7eca8950e35e799f0dc7f275dc1134
tenebrae <dtenebrae@gmail.com>
2022-07-06 10:55:16 -0400

    working version

--- get-lib.sh
+++ get-lib.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 # simple script to get all libs included in c header file and pipe it to dnf provide command
 
+# create an array
+declare -a arr=()
+
 # get path from script argument
 CAT_PATH=$1
 
 # parse included modules to search with dnf
 LIB_LIST=$(cat $CAT_PATH | grep "#include" | awk -F ' ' '{print $2}' | sed 's:^.\(.*\).$:\1:')
 
+printf "============= found in .h ================\n\n"
+
 # iterate over list of lists and put it to dnf provide
 for lib in $LIB_LIST ; do
     if [[ $lib != std* ]] ; then
         printf $lib\\n
+        arr+=( $(dnf provides '*'$lib | tail -n 5 | head -n 1 | grep -Po '^.+?(?=-\d)') )
     fi
 done
+
+printf "\n"
+printf "=========== related packages =============\n\n"
+# print unique values
+printf "%s\n" "${arr[@]}" | sort -u
+printf "\n"
